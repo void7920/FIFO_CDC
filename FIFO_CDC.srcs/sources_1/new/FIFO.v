bef374086557f11969f8f9104bb765d9a86d6a15
Seok Hoon Choi <11285743+void7920@users.noreply.github.com>
2021-12-29 00:52:58 +0900

    Combine Module

--- FIFO_CDC.srcs/sources_1/new/FIFO.v
+++ FIFO_CDC.srcs/sources_1/new/FIFO.v
@@ -21,6 +21,50 @@
 
 
 module FIFO #(parameter MSB = 8, parameter addrsize = 8)(
-    
+    rdata,
+    full,
+    empty,
+    wclken,
+    raddr,
+    waddr,
+    rptr,
+    wptr,
+    wq2_rptr,
+    rq2_wptr,
+    wdata,
+    wclk, 
+    wrst_n, 
+    winc,
+    rclk, 
+    rrst_n, 
+    rinc
     );
+    
+    output [MSB-1:0]rdata;
+    output full;
+    output empty;
+    output wclken;
+    output [depth-1:0]raddr;
+    output [depth-1:0]waddr;
+    output [depth:0]rptr;
+    output [depth:0]wptr;    
+    output [depth:0]wq2_rptr;
+    output [depth:0]rq2_wptr;
+    input [MSB-1:0]wdata;
+    input wclk;
+    input wrst_n;
+    input winc;
+    input rclk;
+    input rrst_n;
+    input rinc;
+    
+    assign wclken = ~full & winc;
+    
+    Dual_Port_Ram #(MSB, addrsize) ram(.rd(rdata), .clk(wclk), .wd(wdata), .ra(raddr), .wa(waddr), .we(wclken));
+    Address_Pointer #(addrsize) Write_Pointer(.addr(waddr), .ptr(wptr), .clk(wclk), .rst_n(wrst_n), .state(full), .c(winc));
+    Flag_Full #(addrsize) Full_Flag(.full(full), .clk(wclk), .rst_n(wrst_n), .ptr(wptr), .q2_ptr(wq2_rptr));
+    Address_Pointer #(addrsize) Read_Pointer(.addr(raddr), .ptr(rptr), .clk(rclk), .rst_n(rrst_n), .state(empty), .c(rinc));
+    Flag_Empty #(addrsize) Empty_Flag(.empty(empty),.clk(rclk), .rst_n(rrst_n), .ptr(rptr), .q2_ptr(rq2_wptr));
+    Synchronizer #(addrsize) sync_wq2(.q2_ptr(wq2_rptr), .clk(wclk), .rst_n(wrst_n), .ptr());
+    Synchronizer #(addrsize) sync_rq2(.q2_ptr(rq2_wptr), .clk(rclk), .rst_n(rrst_n), .ptr(wptr));
 endmodule
